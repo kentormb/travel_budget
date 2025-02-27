9f7e38ffab4e6f0fa0a832b1703d4c9ecff26591
Marios <kentormb@gmail.com>
2025-02-27 17:33:31 +0200

    Update cache version

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const CACHE_NAME = "travel-expenses-cache-v9";
+const CACHE_NAME = "travel-expenses-cache-v11";
 
 /// Immediately take control of pages
 self.addEventListener("install", (event) => {
