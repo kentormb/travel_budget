6b0fb47767be91fe30012c76041a6fd009b30ce4
Marios <kentormb@gmail.com>
2025-04-02 09:40:09 +0300

    Filter expense name list

--- src/components/expense/ExpenseNameInput.tsx
+++ src/components/expense/ExpenseNameInput.tsx
@@ -12,6 +12,7 @@ interface ExpenseNameInputProps {
 export function ExpenseNameInput({ value, onChange, categoryID }: ExpenseNameInputProps) {
     const [inputValue, setInputValue] = useState(value);
     const [namesArray, setNamesArray] = useState<string[]>([]);
+    const [filteredNamesArray, setFilteredNamesArray] = useState<string[]>([]);
 
     useEffect(() => {
         const selectedTripId = localStorage.getItem("selectedTripId");
@@ -28,6 +29,7 @@ export function ExpenseNameInput({ value, onChange, categoryID }: ExpenseNameInp
                     )
                 );
                 setNamesArray(uniqueNames);
+                setFilteredNamesArray(uniqueNames);
             }
         }
     }, [categoryID]);
@@ -40,6 +42,15 @@ export function ExpenseNameInput({ value, onChange, categoryID }: ExpenseNameInp
                 required
                 value={inputValue}
                 onChange={(e) => {
+                    if (!e.target.value) {
+                        console.log(namesArray)
+                        setFilteredNamesArray(namesArray);
+                    } else {
+                        const filteredNames = namesArray.filter((name) =>
+                            name.toLowerCase().includes(e.target.value.toLowerCase())
+                        );
+                        setFilteredNamesArray(filteredNames);
+                    }
                     setInputValue(e.target.value);
                     onChange(e.target.value);
                 }}
@@ -47,12 +58,8 @@ export function ExpenseNameInput({ value, onChange, categoryID }: ExpenseNameInp
             />
 
             <div className="max-w-[calc(100vw-50px)] overflow-x-scroll pb-[7px]">
-                <div className="flex flex-row flex-nowrap gap-2
-
-
-
-                ">
-                    {namesArray.map((name) => (
+                <div className="flex flex-row flex-nowrap gap-2">
+                    {filteredNamesArray.map((name) => (
                         <Badge
                             key={name}
                             className="cursor-pointer px-2 py-1 rounded-md bg-gray-100 text-gray-500 hover:bg-gray-300 transition whitespace-nowrap"
