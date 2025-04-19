bec4ba7391a1d93f83e2ef3b6db1df5a097eab02
Marios <kentormb@gmail.com>
2025-04-19 16:57:44 +0300

    Update logo

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const CACHE_NAME = "travel-expenses-cache-v35";
+const CACHE_NAME = "travel-expenses-cache-v36";
 
 /// Immediately take control of pages
 self.addEventListener("install", (event) => {
